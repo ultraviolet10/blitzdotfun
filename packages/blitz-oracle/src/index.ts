a99309986c8711752aefbfda9ca127f35d869386
ultraviolet10 <aritrac1998@hotmail.com>
2025-08-28 16:29:32 +0530

    feat: add formatting (use only within blitz-oracle for now)

--- packages/blitz-oracle/src/index.ts
+++ packages/blitz-oracle/src/index.ts
@@ -2,5 +2,5 @@ import { app } from "./server/index"
 
 export default {
     // cf workers define the app port in wrangler.jsonc, so we don't need it here
-    fetch: app.fetch
+    fetch: app.fetch,
 }
