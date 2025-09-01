0efc1b5eccf8ca4750a2d78bfd61ec5f69163060
kshitij-hash <kjangra1557@gmail.com>
2025-09-01 17:31:23 +0530

    fsd

--- packages/mini-app/src/components/ui/tabs/index.ts
+++ packages/mini-app/src/components/ui/tabs/index.ts
@@ -1,4 +1,4 @@
 export { HomeTab } from './HomeTab';
 export { ActionsTab } from './ActionsTab';
 export { ProfileTab } from './ProfileTab';
-export { WalletTab } from './WalletTab'; 
+// export { WalletTab } from './WalletTab'; 
