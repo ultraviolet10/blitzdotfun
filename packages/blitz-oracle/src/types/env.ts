614041c76ea1b83c7a33c172abba2d3c38cca5d7
ultraviolet10 <aritrac1998@hotmail.com>
2025-09-09 03:05:40 +0530

    byebye

--- packages/blitz-oracle/src/types/env.ts
+++
@@ -1,9 +0,0 @@
-// Environment variable types for Cloudflare Workers
-export interface CloudflareBindings {
-    FIREBASE_PROJECT_ID?: string // Optional - will use service account file if not provided
-    FIREBASE_DATABASE_URL?: string // Optional - will use default URL if not provided
-
-    // Existing environment variables
-    APP_PORT: string
-    PRIVATE_KEY_BLITZ?: string
-}
--- packages/mini-app/src/components/ui/tabs/index.ts
+++
@@ -1,4 +0,0 @@
-export { HomeTab } from './HomeTab';
-export { ActionsTab } from './ActionsTab';
-export { ProfileTab } from './ProfileTab';
-export { WalletTab } from './WalletTab'; 
